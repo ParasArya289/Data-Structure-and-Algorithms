377cd8470a016013246c7b1b4d57331fa00f3d67
ParasArya289 <parasarya289@gmail.com>
2022-07-10 11:50:50 +0530

    2d array

---
+++ 2D Arrays/2dCharacterArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	char a[][10]={
+		"Paras",
+		"Arya",
+		"Manish",
+		"Kansana",
+	} ;
+
+	for(int i=0;i<4;i++){
+	cout<<a[i]<<endl;
+
+	}
+}
