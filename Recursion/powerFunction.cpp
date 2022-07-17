d833d6830793f4716cbcd4acbafa5ea557043725
ParasArya289 <parasarya289@gmail.com>
2022-07-17 11:18:55 +0530

    Recursion

--- Recursion/powerFunction.cpp
+++ Recursion/powerFunction.cpp
@@ -8,10 +8,24 @@ int power(int num, int pow){
 	return num * power(num, pow-1);
 }
 
+int fastPower(int num, int pow){
+	if(pow == 0){
+		return 1;
+	}
+	int subProb = fastPower(num,pow/2);
+	int subProbsq = subProb*subProb;
+
+	if(pow&1){
+		return num * subProbsq;
+	}
+	return subProbsq;
+}
+
 int main(){
 	int num,pow;
 	cin>>num>>pow;
 
-	cout<<power(num,pow);
+	cout<<power(num,pow)<<endl;
+	cout<<fastPower(num,pow)<<endl;
 
 }
